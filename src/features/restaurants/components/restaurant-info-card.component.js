1eaa5b073c28c4b54aaa96cb31bc5e8fd95f5fb7
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-05-06 11:20:58 -0700

    Implemented styled-components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import styled from "styled-components/native";
+
+const RestaurantCard = styled(Card)``;
+
+const RestaurantCover = styled(Card.Cover)`
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+`;
+
+const Title = styled.Text`
+  padding: 16px;
+  color: red;
+`;
 
 export default function RestaurantInfoCard({ restaurant = {} }) {
   const {
@@ -16,16 +29,9 @@ export default function RestaurantInfoCard({ restaurant = {} }) {
   } = restaurant;
 
   return (
-    <Card elevation={5}>
-      <Card.Cover key={name} source={{ uri: photos[0] }} style={styles.cover} />
-      <Card.Title title={name} />
-    </Card>
+    <RestaurantCard elevation={5}>
+      <RestaurantCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
   );
 }
-
-const styles = StyleSheet.create({
-  cover: {
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
-  },
-});
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -2,42 +2,41 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 import { Searchbar } from "react-native-paper";
 import RestaurantInfoCard from "../components/restaurant-info-card.component";
+import styled from "styled-components/native";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+
+const SearchbarEl = styled(Searchbar)`
+  border-radius: 8px;
+`;
+
+const RestaurantListContainer = styled.View`
+  flexgrow: 1;
+  padding: 16px;
+`;
 
 export default function RestaurantsScreen() {
   const [search, setSearch] = useState("");
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
-        <Searchbar
+    <SafeArea>
+      <SearchContainer>
+        <SearchbarEl
           placeholder="Search restaurants"
           onChangeText={setSearch}
           value={search}
-          style={styles.searchbar}
         />
-      </View>
-      <View style={styles.list}>
+      </SearchContainer>
+      <RestaurantListContainer>
         <RestaurantInfoCard />
-      </View>
-    </SafeAreaView>
+      </RestaurantListContainer>
+    </SafeArea>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-  searchbar: {
-    borderRadius: 8,
-  },
-  list: {
-    flexGrow: 1,
-    padding: 16,
-    // backgroundColor: "blue",
-  },
-});
