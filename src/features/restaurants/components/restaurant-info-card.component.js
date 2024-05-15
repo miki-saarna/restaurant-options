84b8f993bf6c5f488ba03b3127f69f59ca6b5c9e
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-05-14 21:41:41 -0700

    text style utils

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,23 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { Image } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
 
 const RestaurantCard = styled(Card)``;
 
 const RestaurantCover = styled(Card.Cover)`
   border-bottom-left-radius: ${(props) => props.theme.space[0]};
   border-bottom-right-radius: ${(props) => props.theme.space[0]};
-`;
-
-const Title = styled.Text`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.text.primary};
 `;
 
 const Address = styled.Text`
@@ -41,17 +36,27 @@ const Section = styled.View`
   align-items: center;
 `;
 
+const SectionEnd = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Icon = styled.Image`
+  width: 20px;
+  height: 20px;
+`;
+
 export default function RestaurantInfoCard({ restaurant = {} }) {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const RatingArray = () => {
@@ -64,22 +69,22 @@ export default function RestaurantInfoCard({ restaurant = {} }) {
     <RestaurantCard elevation={5}>
       <RestaurantCover key={name} source={{ uri: photos[0] }} />
       <Info>
-        <Title>{name}</Title>
+        <Text variant="label">{name}</Text>
         <Section>
           <Rating>
             <RatingArray />
           </Rating>
-          {isClosedTemporarily && (
-            <Text variant="label" style={{ color: "red" }}>
-              CLOSED TEMPORARILY
-            </Text>
-          )}
-          <Spacer position="left" size="large">
-            {isOpenNow && <SvgXml xml={open} height={20} width={20} />}
-          </Spacer>
-          <Spacer poisition="left" size="large">
-            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
-          </Spacer>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="error">CLOSED TEMPORARILY</Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} height={20} width={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Icon source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
         </Section>
         <Address>{address}</Address>
       </Info>
