c35ec9117b413662ba808b2ac35e6084cdbe5026
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-05-17 11:34:12 -0700

    Init loading indicator

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { Searchbar } from "react-native-paper";
+import { Searchbar, ActivityIndicator } from "react-native-paper";
 import RestaurantInfoCard from "../components/restaurant-info-card.component";
 import styled from "styled-components/native";
 import { SafeArea } from "../../../components/utils/safe-area.component";
@@ -19,6 +19,12 @@ const RestaurantList = styled.FlatList.attrs({
   },
 })``;
 
+const LoadingIndicatorContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  background-color: #00000040;
+`;
+
 export default function RestaurantsScreen() {
   const { restaurants, isLoading, error, setLocation } =
     useContext(RestaurantsContext);
@@ -34,11 +40,17 @@ export default function RestaurantsScreen() {
           value={search}
         />
       </SearchContainer>
-      <RestaurantList
-        data={restaurants}
-        renderItem={({ item }) => <RestaurantInfoCard restaurant={item} />}
-        keyExtractor={(item) => item.name}
-      />
+      {isLoading ? (
+        <LoadingIndicatorContainer>
+          <ActivityIndicator animation={true} color="tomato" size="large" />
+        </LoadingIndicatorContainer>
+      ) : (
+        <RestaurantList
+          data={restaurants}
+          renderItem={({ item }) => <RestaurantInfoCard restaurant={item} />}
+          keyExtractor={(item) => item.name}
+        />
+      )}
     </SafeArea>
   );
 }
