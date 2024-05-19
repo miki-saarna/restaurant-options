74235047049d26c04f3080ad972f6033b29af415
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-05-19 14:19:47 -0700

    Search disclaimer message

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,19 +1,37 @@
-import React, { useContext, useState } from "react";
 import styled from "styled-components/native";
+import React, { useContext, useState } from "react";
+import { FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { LocationContext } from "../../../services/location/location.context";
 
 const SearchContainer = styled.View`
-  padding: ${(props) => props.theme.space[3]};
+  padding-top: ${(props) => props.theme.space[3]};
+  padding-right: ${(props) => props.theme.space[3]};
+  padding-left: ${(props) => props.theme.space[3]};
 `;
 
 const SearchbarEl = styled(Searchbar)`
   border-radius: ${(props) => props.theme.space[2]};
 `;
 
+const DisclaimerContainer = styled.View`
+  margin-top: ${(props) => props.theme.space[3]};
+  padding: ${(props) => props.theme.space[3]};
+  border-radius: ${(props) => props.theme.space[2]};
+  border: 1px solid #c6e2ff;
+  background-color: #ecf5ff;
+`;
+
+const DisclaimerText = styled.Text`
+  color: #111827;
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
 export const Search = () => {
   const { keyword, onSearch } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
+
+  const validCities = ["Antwerp", "Chicago", "San Francisco", "Toronto"];
 
   return (
     <SearchContainer>
@@ -25,6 +43,16 @@ export const Search = () => {
           onSearch(searchKeyword);
         }}
       />
+      <DisclaimerContainer>
+        <DisclaimerText>
+          Search results currently only available for the following cities:
+        </DisclaimerText>
+        <FlatList
+          data={validCities}
+          renderItem={({ item }) => <DisclaimerText>- {item}</DisclaimerText>}
+          keyExtractor={(item) => item}
+        />
+      </DisclaimerContainer>
     </SearchContainer>
   );
 };
